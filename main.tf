0322a36c6b9632278ed78ef5ada30fd72c5da344
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-08-26 21:54:05 -0400

    Removing S3 Bucket from main.tf to correct state drift from tf state file.

--- main.tf
+++ main.tf
@@ -13,14 +13,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Define a resource to create - in this case, an S3 bucket
-resource "aws_s3_bucket" "my_first_bucket" {
-  # Bucket names must be globally unique. We use a variable and a random suffix.
-  bucket = "${var.bucket_name_prefix}-s3-bucket"
-
-  tags = {
-    Name        = "My first Terraform bucket"
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-  }
-}
