9e3d66459f17d2ac367fbc8454ffe5555cdf47ac
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-08-27 12:23:18 -0400

    Configure S3 remote state backend

---
+++ backend.tf
@@ -0,0 +1,18 @@
+# .github/backend.tf
+
+terraform {
+  backend "s3" {
+    # Replace with your unique S3 bucket name
+    bucket = "your-initials-tf-state-bucket"
+
+    # This key defines the "folder" structure inside the S3 bucket for your state file
+    key = "global/s3/terraform.tfstate"
+
+    region = "us-east-1"
+
+    # The DynamoDB table for state locking
+    dynamodb_table = "terraform-state-lock-table"
+
+    encrypt = true
+  }
+}
