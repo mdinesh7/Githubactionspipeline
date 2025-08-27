5448130d5ade12a7e49224f41da968f48abd3502
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-08-26 21:55:20 -0400

    Removing output for s3 from outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,1 @@
 # Defines an output value for the S3 bucket's name
-output "s3_bucket_name" {
-  description = "The final, full name of the S3 bucket created."
-  value       = aws_s3_bucket.my_first_bucket.bucket
-}
