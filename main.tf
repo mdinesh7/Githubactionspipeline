969350fbff818e64c90bdaa29691026967d45038
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-08-31 17:53:47 -0400

    Fix1: Replace deprecated acl with public access block

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_ownership_controls" "avatars" {
   }
 }
 
-resource "aws_s_3_bucket_public_access_block" "avatars" {
+resource "aws_s3_bucket_public_access_block" "avatars" {
   bucket = aws_s3_bucket.avatars.id
 
   block_public_acls       = true
