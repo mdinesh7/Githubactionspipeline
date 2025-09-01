54d01fe48bfd552f003a29a11842c1263d5c43d0
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-09-01 11:59:39 -0400

    passing proper values

--- modules/s3-bucket/outputs.tf
+++ modules/s3-bucket/outputs.tf
@@ -1,5 +1,5 @@
 # Defines an output value for the S3 bucket's name
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.avatars.arn
+  value       = aws_s3_bucket.this.arn
 }
