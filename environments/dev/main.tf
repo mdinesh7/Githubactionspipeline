3e6c2497f34337394e857dbd72fc91e07872ad55
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-09-01 14:25:07 -0400

    Destroying s3 not needed anymore

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,13 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-module "avatar_bucket" {
-  source = "../../modules/s3-bucket" # <-- Relative path to the module
-
-  bucket_name = "my-app-dev-avatars-12345" # Unique name
-  tags = {
-    Environment = "dev"
-    ManagedBy   = "Terraform"
-  }
-}
